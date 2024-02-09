928ff549c0fd343aec11f6aa50d3d61ed6a89e7d
Ted Slesinski <ted@slesinski.org>
2024-02-09 15:54:17 -0500

    Update init_environment.sh

--- script/init_environment.sh
+++ script/init_environment.sh
@@ -15,7 +15,7 @@ fi
 pacman -S --noconfirm ruby2.7 ruby-bundler
 
 # Setting default ruby version
-cp /usr/bin/ruby-2.7 /usr/bin/ruby
+cp /usr/bin/ruby-3.0 /usr/bin/ruby
 
 # debug
 ruby -v && bundle version
