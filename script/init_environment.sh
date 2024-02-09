48f908dfe30262b7bcbf184fa74a5e649d8f6b50
Ted Slesinski <ted@slesinski.org>
2024-02-09 17:30:51 -0500

    Update init_environment.sh

--- script/init_environment.sh
+++ script/init_environment.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-# Update packages database
+# Update packages database and upgrade the system
 pacman -Syu --noconfirm
 
-# Installing git package
+# Install essential packages
 pacman -S --noconfirm git
 
-# Installing openssh package
+# Conditionally install openssh if SSH_PRIVATE_KEY is set
 if [[ -n "${SSH_PRIVATE_KEY}" ]]; then
   pacman -S --noconfirm openssh
 fi
 
-# Installing ruby libraries
-pacman -S --noconfirm ruby ruby-bundler
+# Install Ruby
+pacman -S --noconfirm ruby
 
-# Setting default ruby version
-# cp /usr/bin/ruby-3.0 /usr/bin/ruby
+# Ensure gem is up to date
+gem update --system
 
-# debug
-ruby -v && bundle version
+# Uninstall any existing versions of bundler
+gem uninstall bundler -a -x || true
+
+# Install specific version of Bundler
+gem install bundler -v 2.5.5
+
+# Verify installations
+ruby -v # Should show the version of Ruby installed. You aim for Ruby 3.0, but this will show the latest version installed via pacman.
+bundle -v # Should show Bundler version 2.5.5
 
 # This is a temporary workaround
 # See https://github.com/actions/checkout/issues/766
